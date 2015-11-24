2a1291cbb6ee1fde317ac00080530a9abb8e005a
douglasren <shumengren2010@gmail.com>
2015-11-24 17:24:47 +0800

    完成交互

---
+++ Application/Home/Library/BaseUtil.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: teddy
+ * Date: 11/24/15
+ * Time: 1:45 PM
+ */
+class BaseUtil
+{
+    
+}
---
+++ Application/Home/Library/Encode.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: teddy
+ * Date: 11/24/15
+ * Time: 3:53 PM
+ */
+
+namespace Home\Library;
+
+
+class encode {
+
+    static
+    
+}
